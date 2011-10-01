628aa054a26e012949311f4be8a7ad3988329921
Marc Hoersken <info@marc-hoersken.de>
2011-10-01 11:32:51 -0700

    Added methods to modify AMF content

--- src/de/uxnr/amf/AMF.java
+++ src/de/uxnr/amf/AMF.java
@@ -19,61 +19,85 @@ public class AMF {
 		AMF3.register();
 		Flex.register();
 	}
-	
-	private List<AMF_Header> headers = new Vector<AMF_Header>();
-	private List<AMF_Message> messages = new Vector<AMF_Message>();
-	
+
+	private final List<AMF_Header> headers = new Vector<AMF_Header>();
+	private final List<AMF_Message> messages = new Vector<AMF_Message>();
+
 	public AMF() { }
-	
+
 	public AMF(InputStream input) throws IOException {
 		this.read(input);
 	}
-	
+
 	public void read(InputStream stream) throws IOException {
 		DataInputStream input = new DataInputStream(stream);
 		AMF_Context context = new AMF_Context();
-		
+
 		U16 version = new U16(context, input);
 		if (version.get() != 0 && version.get() != 3)
 			throw new IOException("Unsupported AMF version");
-		
+
 		U16 headers = new U16(context, input);
 		for (int index = 0; index < headers.get(); index++)
 			this.headers.add(new AMF_Header(input));
-		
+
 		U16 messages = new U16(context, input);
 		for (int index = 0; index < messages.get(); index++)
 			this.messages.add(new AMF_Message(input));
 	}
-	
+
 	public void write(OutputStream stream) throws IOException {
 		DataOutputStream output = new DataOutputStream(stream);
 		AMF_Context context = new AMF_Context();
-		
+
 		U16 version = new U16(0);
 		version.write(context, output);
-		
+
 		U16 headers = new U16(this.headers.size());
 		headers.write(context, output);
 		for (AMF_Header header : this.headers) {
 			header.write(output);
 		}
-		
+
 		U16 messages = new U16(this.messages.size());
 		messages.write(context, output);
 		for (AMF_Message message : this.messages) {
 			message.write(output);
 		}
 	}
-	
+
 	public List<AMF_Header> getHeaders() {
 		return this.headers;
 	}
-	
+
 	public List<AMF_Message> getMessages() {
 		return this.messages;
 	}
-	
+
+	public void addHeader(AMF_Header header) {
+		this.headers.add(header);
+	}
+
+	public void addMessage(AMF_Message message) {
+		this.messages.add(message);
+	}
+
+	public void removeHeader(AMF_Header header) {
+		this.headers.remove(header);
+	}
+
+	public void removeHeader(int index) {
+		this.headers.remove(index);
+	}
+
+	public void removeMessage(AMF_Message message) {
+		this.messages.remove(message);
+	}
+
+	public void removeMessage(int index) {
+		this.messages.remove(index);
+	}
+
 	@Override
 	public String toString() {
 		String str = "AMF\n";
