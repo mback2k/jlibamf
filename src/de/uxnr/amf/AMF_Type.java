87c26eec2df3236b0fbf62f323303ba3310b0748
Marc Hoersken <info@marc-hoersken.de>
2011-10-01 11:35:04 -0700

    Added missing annotation

--- src/de/uxnr/amf/AMF_Type.java
+++ src/de/uxnr/amf/AMF_Type.java
@@ -7,8 +7,13 @@ import java.io.IOException;
 public interface AMF_Type {
 	public void write(AMF_Context context, DataOutputStream output) throws IOException;
 	public AMF_Type read(AMF_Context context, DataInputStream input) throws IOException;
-	
+
+	@Override
 	public String toString();
+
+	@Override
 	public int hashCode();
+
+	@Override
 	public boolean equals(Object obj);
 }
