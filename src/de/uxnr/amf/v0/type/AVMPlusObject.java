24befc1fcbe62cf2c26a540caf25b634b420de6e
Marc Hoersken <info@marc-hoersken.de>
2011-10-01 13:31:14 -0700

    Store AMF3_Type inside AVMPlusObject to keep complete object tree valid

--- src/de/uxnr/amf/v0/type/AVMPlusObject.java
+++ src/de/uxnr/amf/v0/type/AVMPlusObject.java
@@ -1,15 +1,34 @@
 package de.uxnr.amf.v0.type;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 import de.uxnr.amf.AMF_Context;
 import de.uxnr.amf.AMF_Type;
+import de.uxnr.amf.v0.AMF0_Type;
 import de.uxnr.amf.v3.AMF3_Type;
 
-public class AVMPlusObject extends EmptyType {
+public class AVMPlusObject extends AMF0_Type {
+	private AMF3_Type value = null;
+
+	@Override
+	public void write(AMF_Context context, DataOutputStream output) throws IOException {
+		AMF3_Type.writeType(context, output, this.value);
+	}
+
 	@Override
 	public AMF_Type read(AMF_Context context, DataInputStream input) throws IOException {
-		return AMF3_Type.readType(context, input);
+		this.value = AMF3_Type.readType(context, input);
+
+		return this;
+	}
+
+	public void set(AMF3_Type value) {
+		this.value = value;
+	}
+
+	public AMF3_Type get() {
+		return this.value;
 	}
 }
