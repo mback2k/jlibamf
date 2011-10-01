7c8d686c2726d21074da814e2ebce444a92e78cc
Marc Hoersken <info@marc-hoersken.de>
2011-10-01 13:38:52 -0700

    Updated basic AMF test to include writing

--- src/de/uxnr/amf/test/AMFTest.java
+++ src/de/uxnr/amf/test/AMFTest.java
@@ -6,6 +6,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import de.uxnr.amf.AMF;
@@ -14,17 +15,17 @@ import de.uxnr.amf.v3.base.U29;
 import de.uxnr.amf.v3.type.Integer;
 
 public class AMFTest {
-	public static void main(String[] args) throws IOException {		
+	public static void main(String[] args) throws IOException {
 		for (int i = 0; i < 1000; i++) {
 			long value = (long) (Integer.MAX_VALUE - (Math.random() * Integer.MAX_VALUE * 2));
 			test(value, true);
 		}
-		
+
 		for (int i = 0; i < 1000; i++) {
 			long value = (long) (Math.random() * U29.MAX_VALUE);
 			test(value, false);
 		}
-		
+
 		File folder = new File(".");
 		for (File file : folder.listFiles()) {
 			if (file.getName().endsWith(".amf")) {
@@ -32,40 +33,51 @@ public class AMFTest {
 			}
 		}
 	}
-	
+
 	public static void test(long value, boolean signed) throws IOException {
 		AMF_Context context = new AMF_Context();
 		Integer test = new Integer();
 		test.signed = signed;
-		
+
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		
+
 		test.set(value);
 		test.write(context, new DataOutputStream(out));
-		
+
 		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-		
+
 		test.read(context, new DataInputStream(in));
-		
+
 		System.out.println(value+" => "+test.get());
-		
+
 		if (value != test.get())
 			throw new RuntimeException("Test failed!");
 	}
-	
+
 	public static void test(File file) throws IOException {
 		FileInputStream input = new FileInputStream(file);
-		
+
 		System.out.println(file.length());
 		System.out.println(input.available());
-		
+
 		AMF amf = new AMF(input);
-		
+
 		System.out.println(amf.getHeaders().size());
 		System.out.println(amf.getMessages().size());
 		System.out.println(input.available());
-		
+
 		if (input.available() != 0)
 			throw new RuntimeException("Test failed!");
+
+		file = File.createTempFile("amf", file.getName());
+		FileOutputStream out = new FileOutputStream(file);
+
+		amf.write(out);
+
+		out.close();
+
+		FileInputStream in = new FileInputStream(file);
+
+		amf = new AMF(in);
 	}
 }
