25cfb89fb57e8717eaac1243e20daa31e06e0cca
Marc Hoersken <info@marc-hoersken.de>
2011-10-01 11:34:04 -0700

    Added methods to create AMF references using AMF context

--- src/de/uxnr/amf/AMF_Context.java
+++ src/de/uxnr/amf/AMF_Context.java
@@ -9,40 +9,60 @@ import de.uxnr.amf.v3.AMF3_Type;
 import de.uxnr.amf.v3.base.UTF8;
 
 public class AMF_Context {
-	private List<AMF0_Type> amf0objects = new Vector<AMF0_Type>();
-	private List<AMF3_Type> amf3objects = new Vector<AMF3_Type>();
-	private List<AMF3_Trait> amf3trait = new Vector<AMF3_Trait>();
-	private List<UTF8> amf3strings = new Vector<UTF8>();
-	
-	public void addAMF0Object(AMF0_Type value) {
+	private final List<AMF0_Type> amf0objects = new Vector<AMF0_Type>();
+	private final List<AMF3_Type> amf3objects = new Vector<AMF3_Type>();
+	private final List<AMF3_Trait> amf3trait = new Vector<AMF3_Trait>();
+	private final List<UTF8> amf3strings = new Vector<UTF8>();
+
+	public long addAMF0Object(AMF0_Type value) {
 		this.amf0objects.add(value);
+		return this.amf0objects.indexOf(value);
 	}
-	
+
 	public AMF0_Type getAMF0Object(long index) {
 		return this.amf0objects.get((int) index);
 	}
-	
-	public void addAMF3Object(AMF3_Type value) {
-		this.amf3objects.add(value);
+
+	public long getAMF0ObjectReference(AMF0_Type value) {
+		return this.amf0objects.indexOf(value);
 	}
-	
+
+	public long addAMF3Object(AMF3_Type value) {
+		this.amf3objects.add(value);
+		return this.amf3objects.indexOf(value);
+	}
+
 	public AMF3_Type getAMF3Object(long index) {
 		return this.amf3objects.get((int) index);
 	}
-	
-	public void addAMF3Trait(AMF3_Trait value) {
-		this.amf3trait.add(value);
+
+	public long getAMF3ObjectReference(AMF3_Type value) {
+		return this.amf3objects.indexOf(value);
 	}
-	
+
+	public long addAMF3Trait(AMF3_Trait value) {
+		this.amf3trait.add(value);
+		return this.amf3trait.indexOf(value);
+	}
+
 	public AMF3_Trait getAMF3Trait(long index) {
 		return this.amf3trait.get((int) index);
 	}
-	
-	public void addAMF3String(UTF8 value) {
-		this.amf3strings.add(value);
+
+	public long getAMF3TraitReference(AMF3_Trait value) {
+		return this.amf3trait.indexOf(value);
 	}
-	
+
+	public long addAMF3String(UTF8 value) {
+		this.amf3strings.add(value);
+		return this.amf3strings.indexOf(value);
+	}
+
 	public UTF8 getAMF3String(long index) {
 		return this.amf3strings.get((int) index);
+	}
+
+	public long getAMF3StringReference(UTF8 value) {
+		return this.amf3strings.indexOf(value);
 	}
 }
