5ab873ca698104993976977b2223bc36cad89b7a
Marc Hoersken <info@marc-hoersken.de>
2011-10-10 23:34:28 -0700

    Added Boolean support to custom object classes Added type checks before casting objects

--- src/de/uxnr/amf/v3/AMF3_Object.java
+++ src/de/uxnr/amf/v3/AMF3_Object.java
@@ -12,9 +12,13 @@ import java.util.Collection;
 import de.uxnr.amf.AMF_Context;
 import de.uxnr.amf.v3.type.Array;
 import de.uxnr.amf.v3.type.Double;
+import de.uxnr.amf.v3.type.False;
 import de.uxnr.amf.v3.type.Integer;
+import de.uxnr.amf.v3.type.Null;
 import de.uxnr.amf.v3.type.Object;
 import de.uxnr.amf.v3.type.String;
+import de.uxnr.amf.v3.type.True;
+import de.uxnr.amf.v3.type.Undefined;
 
 public abstract class AMF3_Object extends AMF3_Type {
 	private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
@@ -49,38 +53,109 @@ public abstract class AMF3_Object extends AMF3_Type {
 			java.lang.Object fieldValue = field.get(this);
 
 			try {
+				AMF3_Type data = object.get(fieldName);
+
+				if (data instanceof Null || data instanceof Undefined) {
+					if (java.lang.Object.class.isAssignableFrom(field.getType())) {
+						field.set(this, null);
+					}
+					continue;
+				}
+
 				if (field.getType() == Object.class) {
-					Object value = (Object) object.get(fieldName);
+					Object value = null;
+
+					if (data instanceof Object) {
+						value = (Object) data;
+					}
+
 					this.propertyChangeSupport.firePropertyChange(fieldName, fieldValue, value);
 					field.set(this, value);
 
 				} else if (field.getType() == Array.class) {
-					Array value = (Array) ((Object) object.get(fieldName)).getExternal();
+					Array value = null;
+
+					if (data instanceof Array) {
+						value = (Array) data;
+
+					} else if (data instanceof Object) {
+						AMF3_Type external = ((Object) data).getExternal();
+
+						if (external instanceof Array) {
+							value = (Array) external;
+						}
+					}
+
 					this.propertyChangeSupport.firePropertyChange(fieldName, fieldValue, value);
 					field.set(this, value);
 
 				} else if (field.getType() == java.lang.String.class) {
-					java.lang.String value = ((String) object.get(fieldName)).get();
+					java.lang.String value = null;
+
+					if (data instanceof String) {
+						value = ((String) data).get();
+					}
+
 					this.propertyChangeSupport.firePropertyChange(fieldName, fieldValue, value);
 					field.set(this, value);
 
 				} else if (field.getType() == java.lang.Integer.class) {
-					java.lang.Integer value = ((Integer) object.get(fieldName)).get();
+					java.lang.Integer value = null;
+
+					if (data instanceof Integer) {
+						value = ((Integer) data).get();
+					}
+
 					this.propertyChangeSupport.firePropertyChange(fieldName, fieldValue, value);
 					field.set(this, value);
 
 				} else if (field.getType() == java.lang.Double.class) {
-					java.lang.Double value = ((Double) object.get(fieldName)).get();
+					java.lang.Double value = null;
+
+					if (data instanceof Double) {
+						value = ((Double) data).get();
+					}
+
+					this.propertyChangeSupport.firePropertyChange(fieldName, fieldValue, value);
+					field.set(this, value);
+
+				} else if (field.getType() == java.lang.Boolean.class) {
+					java.lang.Boolean value = null;
+
+					if (data instanceof True) {
+						value = true;
+
+					} else if (data instanceof False) {
+						value = false;
+					}
+
 					this.propertyChangeSupport.firePropertyChange(fieldName, fieldValue, value);
 					field.set(this, value);
 
 				} else if (AMF3_Object.class.isAssignableFrom(field.getType())) {
-					AMF3_Object value = (AMF3_Object) object.get(fieldName);
+					AMF3_Object value = null;
+
+					if (data instanceof AMF3_Object) {
+						value = (AMF3_Object) data;
+					}
+
 					this.propertyChangeSupport.firePropertyChange(fieldName, fieldValue, value);
 					field.set(this, value);
 
 				} else if (Collection.class.isAssignableFrom(field.getType())) {
-					Collection<AMF3_Type> value = ((Array) ((Object) object.get(fieldName)).getExternal()).values();
+					Collection<AMF3_Type> value = null;
+
+					if (data instanceof Array) {
+						value = ((Array) data).values();
+
+					} else if (data instanceof Object) {
+						AMF3_Type external = ((Object) data).getExternal();
+
+						if (external instanceof Array) {
+							value = ((Array) external).values();
+						}
+					}
+
 					this.propertyChangeSupport.firePropertyChange(fieldName, fieldValue, value);
 					field.set(this, value);
 
